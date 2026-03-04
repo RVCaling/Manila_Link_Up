dc7916d4d787ff0afbb3451cfbceafa23697b12d
Irish Carpio <IrishCarpio-dev@github.com>
2026-03-04 19:34:31 +0800

    Initial commit - created landing page and log in page, can run in AVD

---
+++ app/src/main/java/com/manilalinkup/app/MainActivity.java
@@ -0,0 +1,38 @@
+package com.manilalinkup.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton loginButton;
+    MaterialButton signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        loginButton = findViewById(R.id.material_button_login);
+        signupButton = findViewById(R.id.material_button_signup);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginActivityIntent);
+            }
+        });
+
+
+    }
+}
